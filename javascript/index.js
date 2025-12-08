8ce5e82adb87253d852da9ba52a4699f59941a68
Mudassir Ahmad <ma6386731@gmail.com>
2025-12-08 15:08:11 +0500

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+// array and arraymethod
+// pop
+// Push
+// shift
+// unshift
+// forEach
+// Map
+// filter;
+// find
+// reduce
+
+
+
+
+
+
+// object and function 
+// arrow function
+// traditional function
+// Object method
+
+// truthy and falsy value
+
+// if else if
+// conditional rendering
+// switch
+// early return pattern
+
+// all loops in javascript
