8e73c9d71c3a125c555a93f486aeeb8c7435874c
Mudassir Ahmad <ma6386731@gmail.com>
2025-12-10 14:59:12 +0500

    another commit

--- html/index.js
+++
@@ -1,9 +0,0 @@
-const togglebutton = document.querySelector('.toggle-theme');
-
-const toggletheme = () => {
-    document.body.classList.toggle('dark-theme');
-}
-
-togglebutton.addEventListener('click',toggletheme);
-
-console.log('Theme toggle script loaded.');
